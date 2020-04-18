773ad496a43db35e45643d083a414d18f18993d1
SCLinDennis <dennis60512@gmail.com>
2020-04-17 17:06:15 -0700

    First commit

---
+++ src/com/codewithDennis/Point.java
@@ -0,0 +1,22 @@
+package com.codewithDennis;
+
+public class Point {
+    private int A;
+    private int B;
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int a) {
+        A = a;
+    }
+
+    public int getB() {
+        return B;
+    }
+
+    public void setB(int b) {
+        B = b;
+    }
+}
---
+++ src/com/codewithDennis/Triangle.java
@@ -0,0 +1,61 @@
+package com.codewithDennis;
+
+public class Triangle {
+//    private String type;
+//    private int height;
+//
+//    public Triangle(String type) {
+//        this.type = type;
+//    }
+//
+//    public Triangle(String type, int height) {
+//        this.type = type;
+//        this.height = height;
+//    }
+//
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
+    public void draw(){
+        System.out.println("PointA=("+getPointA().getA()+','+getPointA().getB()+")");
+        System.out.println("PointB=("+getPointB().getA()+','+getPointB().getB()+")");
+        System.out.println("PointC=("+getPointC().getA()+','+getPointC().getB()+")");
+    }
+
+}
